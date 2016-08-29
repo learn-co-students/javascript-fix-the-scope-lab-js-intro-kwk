a94990744a97431f149119643cb6e4b19306c649
Alyssa Sidebottom <alyssa.m.sidebottom@gmail.com>
2016-08-29 00:37:58 +0000

    Done.

--- index.js
+++ index.js
@@ -17,27 +17,7 @@ function add2(n) {
 
 }
 
-// function funkyFunction() {
-//   var funkyVariable = "FUNKY!";
-//   return function theFunk() {
-//     return funkyVariable;
-//   }
-// }
-// var theFunk = funkyFunction();
 
-
-// var funkyFunction = function() {
-//   var innerVariable = "FUNKY!";
-//   return function theFunk() {
-
-//     return innerVariable;
-//   }
-// }
-// var theFunk = funkyFunction();
-// theFunk();
-
-
-// We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
 
 var funkyFunction = function(){
     return function theFunk(){
